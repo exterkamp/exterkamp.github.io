da7f8f69bf4cfb943fb4dc7a1879b6328c04e8d4
Shane Exterkamp <shaneexterkamp5@gmail.com>
2018-08-23 22:12:42 -0400

    [Refactor] Changed layout of all content and added some more content.

--- exterkamp-app/README.md
+++ exterkamp-app/README.md
@@ -1,1 +1,19 @@
-# React App
+# Exterkamp Dot Codes!
+
+Welcome to the source code of my website!  If you want to hack around on it go ahead and feel free.
+It is a basic react app, so there isn't too much to see, but I hope you have as much fun hacking on it
+as I did making it!
+
+## To run the react app locally
+
+This will run the app locally on port 3000.
+```bash
+$ yarn start
+```
+
+## To deploy the app to gh-pages
+
+This will deploy the app to github pages master branch
+```bash
+$ yarn deploy
+```
--- exterkamp-app/src/components/Splash.jsx
+++ exterkamp-app/src/components/Splash.jsx
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 import '../styles/Splash.css';
 import headshot from '../static/img/edited_2.jpg';
 import party_headshot from '../static/img/portrait_party.jpg';
-import olympus from '../static/img/OLYMPUS_macro.jpg';
+// import olympus from '../static/img/OLYMPUS_macro.jpg';
 import resume from '../static/Resume_2018_v5.pdf';
+import hiking from '../static/img/hiking_shane.jpg';
 
 import TextLoop from 'react-text-loop';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -47,49 +48,57 @@ class Splash extends Component {
                         <div className="about-me-title">
                             Hello <span role="img" aria-label="Hello" alt="">👋</span> I'm Shane.  I build software.
                         </div>
-                        <div className="about-me-content">
+                        <div>
                             <p>
-                                I got a degree in Computer Science from the University of Florida, Go Gators! While I was there I met a lot of great people
-                                and built a lot of fun things!  I was happy to go to a few UF Hackathons i.e. <i>Swamp Hacks</i>!  I focused a lot on Java
-                                in school and also did some android development way back on KitKat.  I also got to work with a really cool 
-                                company <a target="_target" href="https://www.infotechfl.com">InfoTech</a> for my Senior Project with 
-                                the <a target="_target" href="http://www.ippd.ufl.edu/">IPPD</a> program where I got my first taste of the cloud.
+                                I started off writing software in high school and it's been a passion ever since.  It all started by learning visual
+                                basic in <i>Software Engineering 1</i> at Niceville Highschool.  It was there that I learned how to make computers do more
+                                than I ever thought possible and it was then that I knew what I wanted to do with my time!
+                                <br /><br />
+                                Since then I went to the University of Florida to study computer science.  While I was there I met a lot of great people
+                                and built a lot of fun things!  I even got to go to a few UF Hackathons i.e. <i>Swamp Hacks</i>!  During my senior capstone
+                                project I got to work with the local Gainesville company <a target="_target" href="https://www.infotechfl.com">InfoTech</a>.
+                                While I was at UF I focused mostly on Java development and I made a few games, 
+                                like <a target="_target" href="https://github.com/AstroTeam/Astro">Astro</a>!  It was there that I learned a lot about software 
+                                development processes, Agile, and how to write code and have it break right before big presentations.
+                                <br /><br />
+                                After graduating I started working at <a target="_target" href="http://nielsen.com">Nielsen</a>.  While there I worked in Research 
+                                and Development, US Television Audience Measurement, Enterprise Core Technology, and Enterprise Cloud Operations.  This exposure to 
+                                so many areas has given me a very large breadth of experience in technology and moved me away from Java centric development to work 
+                                in Python, Javascript, Ansible, React, Angular, Redux, on AWS, Azure and GCP!  It also exposed me to a lot of great people who taught
+                                me a lot about how to be a better software engineer and corporate citizen.  
+                                <br /><br />
+                                As with every software engineer I am committed to a life of learning and evovling my skills.  I hope it was interesting to read about
+                                my experiences so far, and I hope that it only gets more interesting as time goes on!  If that wasn't enough for you I also have 
+                                a <a target="_target" href={resume}>résumé</a> if that's your cup of tea.  
                             </p>
-                            <p>
-                                After graduating I started working at <a target="_target" href="http://nielsen.com">Nielsen</a>.  I have worked in Research 
-                                and Development, US Television Audience Measurement and Enterprise Core Technology.  This exposure to so many areas has given 
-                                me a very large breadth of experience in technology and moved me away from Java centric development to work in Python, 
-                                Javascript, Ansible, React, Angular, Redux, on AWS, Azure and GCP!  
-                            </p>
-                            <p>
-                                I am just starting in my career as a software developer, and I am always looking for the next thing to learn and
-                                do to make tech everywhere better!  If this little summary of my work history wasn't enough, I have a <a target="_target" href={resume}>
-                                résumé</a> if that's your cup of tea.
-                            </p>
-
                         </div>
                     </div>
+                </div>
+                <div className="about">
+                    <div className="things-title">
+                        And I do other things too! <span role="img" aria-label="Mind Explosion" alt="">🤯</span>
+                    </div>
                     <div className="about-photos">
-                            <img className="camera-photo" alt="" src={olympus}/>
+                            <img className="camera-photo" alt="" src={hiking}/>
                             <div>
-                                {/* <p style={{'text-align':'center', 'font-size': '2.5rem'}}>
-                                    📷 🖌 🎮 🏂
-                                </p> */}
                                 <p>
-                                    Outside of making software I enjoy expressing my creative side.  I love photography, both digitally and on 35mm.  To the
-                                    left is my Olympus OM-2S!  I'm still new to developing my own film, but it is addictive and I am doing it more and more in
-                                    addition to shooting digital.
+                                    Outside of making software I enjoy doing a ton of random other things.  In Alaska I picked up hiking and snowboarding.  In
+                                    Florida I picked up a love of <a target="_target" href="https://en.wikipedia.org/wiki/Disc_golf">frolf</a>.  Sometimes I've been
+                                    known to golf too!  I also like to play games on the PC and Switch!      
                                 </p>
                                 <p>
-                                    I also sometimes paint in acrylic and watercolor.  When not feeling colorful I draw in graphite and occasionally in ink.
+                                    I also have some more esoteric hobbies like mechanical keyboards, being an audiophile, building models (mainly military and 40k), and 
+                                    building my own electronics with Raspberry Pi's and Arduinos.
                                 </p>
                                 <p>
-                                    In addition, I game on PC, read books, play disc golf, and snowboard!
+                                    I also enjoy expressing my creative side.  I love photography, both digitally and on 35mm.  I develop my own 35mm which has been an
+                                    experience in and of itself.  I also sometimes paint in acrylic and watercolor.  When not feeling colorful I draw in graphite and 
+                                    occasionally in ink.
                                 </p>
                             </div>
                     </div>
                                 
-                            </div>
+                </div>
                 <div className="professional">
                     <div className="professional-title">
                         Wanna talk <span role="img" aria-label="Phone" alt="">📞</span>?  I'd love to!
