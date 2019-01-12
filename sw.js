f3d1544bff4bcf815a3852276b1512a6331d2476
Shane Exterkamp <shaneexterkamp5@gmail.com>
2019-01-11 21:49:18 -0800

    Add sw and minimize image footprint.

---
+++ sw.js
@@ -0,0 +1,39 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+// Offline analytics, because why not.
+workbox.googleAnalytics.initialize();
+
+workbox.routing.registerRoute(
+    // Cache that JS
+    /.*\.js/,
+    workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+    // Cache CSS files
+    /.*\.css/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+        // Use a custom cache name
+        cacheName: 'css-cache',
+    })
+);
+  
+workbox.routing.registerRoute(
+    // Cache image files
+    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+        // Use a custom cache name
+        cacheName: 'image-cache',
+        plugins: [
+        new workbox.expiration.Plugin({
+            // Cache all 7 images.
+            maxEntries: 7,
+            // Cache for just 1 day, I won't steal your memory for too long.
+            maxAgeSeconds: 1 * 24 * 60 * 60,
+        })
+        ],
+    })
+);
+
